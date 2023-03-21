f84e67c040b939757158ed5cc22f646197332a1d
Chris Jackson <chris.scott.jackson@gmail.com>
2023-03-21 16:21:50 -0700

    Add index page

---
+++ lib/posts.ts
@@ -0,0 +1,97 @@
+type Post = {
+  title: string,
+  date: string,
+  path: string,
+  external?: string,
+  category: string,
+}
+
+export const posts: Post[] = [
+	{
+		title: 'On Burnout: Lessons Learned',
+		date: '07/19/2022',
+		path: '/work/on-burnout',
+		category: 'Work'
+	},
+	{
+		title: 'Building a blog with Svelte: Dynamic imports For Svelte components',
+		date: '02/17/2021',
+		path: '/svelte/dynamic-imports',
+		category: 'Svelte'
+	},
+	{
+		title: 'Getting Value from End to End Tests',
+		date: '02/03/2021',
+		path: '/testing/getting-value-from-end-to-end-tests',
+		category: 'Testing'
+	},
+	{
+		title: 'Building a blog with Svelte: Fetching images via CDN',
+		date: '12/16/2020',
+		path: '/svelte/images-via-cdn',
+		category: 'Svelte'
+	},
+	{
+		title: "Do's and don't's of writing code review feedback",
+		date: '12/9/2020',
+		path: '/code-review/writing-feedback',
+		external: 'https://dev.to/chrsjxn/do-s-and-don-t-s-of-writing-code-review-feedback-2lcm',
+		category: 'Code review'
+	},
+	{
+		title: 'Building a blog with Svelte: Code splitting',
+		date: '12/2/2020',
+		path: '/svelte/code-splitting',
+		external: 'https://dev.to/chrsjxn/building-a-blog-with-svelte-code-splitting-1mdi',
+		category: 'Svelte'
+	},
+	{
+		title: 'Building a blog with Svelte: Adding Markdown',
+		date: '11/25/2020',
+		path: '/svelte/adding-markdown',
+		external: 'https://dev.to/chrsjxn/building-a-blog-with-svelte-adding-markdown-17k1',
+		category: 'Svelte'
+	},
+	{
+		title: 'Building a simple theme store with Svelte',
+		date: '11/18/2020',
+		path: '/svelte/theme-store',
+		external: 'https://dev.to/chrsjxn/building-a-simple-theme-store-with-svelte-9bo',
+		category: 'Svelte'
+	},
+	{
+		title: 'Three tips to be a better code reviewer today',
+		date: '11/13/2020',
+		path: '/code-review/three-tips',
+		external: 'https://dev.to/chrsjxn/three-tips-to-be-a-better-code-reviewer-today-4g2j',
+		category: 'Code review'
+	},
+	{
+		title: 'Polaroid component demo',
+		date: '11/05/2020',
+		path: '/components/polaroid',
+		category: 'Components'
+	},
+	{
+		title: 'Reviewing code like a junior engineer',
+		date: '11/05/2020',
+		path: '/code-review/like-a-junior',
+		category: 'Code review'
+	}
+];
+
+export const sortedPosts: Record<string, Post[] | undefined> = {};
+export const categories: string[] = [];
+
+for (let i = 0; i < posts.length; i++) {
+	const post = posts[i];
+	const category = post.category;
+
+	if (sortedPosts[category] === undefined) {
+		sortedPosts[category] = [];
+		categories.push(category);
+	}
+
+  // TODO: This function ensures this array exists, but TS doesn't see that
+	(sortedPosts[category] as Post[]).push(post);
+}
