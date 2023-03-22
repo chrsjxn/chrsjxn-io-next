db992b7d022cfd3ffe7d05df13172b0e4b3f40eb
Chris Jackson <chris.scott.jackson@gmail.com>
2023-03-21 21:57:53 -0700

    Update 404 page head

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,8 +1,18 @@
 import { MdxLayout } from "@/components/MdxLayout/MdxLayout";
+import Head from "next/head";
 
 export default function NotFound() {
-  return <MdxLayout>
+  return <>
+  <Head>
+    <title>chrsjxn.io - 404</title>
+    <link
+			rel="icon"
+			href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌊</text></svg>"
+		/>
+  </Head>
+  <MdxLayout>
     <h1>404</h1>
-    <p>Looks like the page you were looking for is on sabbatical.</p>
+    <p>Seems like the page you were looking for is on sabbatical.</p>
   </MdxLayout>
+  </>
 }
