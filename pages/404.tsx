fbab07f88d8f381af4ca279dd1afd591fc7f71b5
Chris Jackson <chris.scott.jackson@gmail.com>
2023-03-21 21:55:30 -0700

    Add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+import { MdxLayout } from "@/components/MdxLayout/MdxLayout";
+
+export default function NotFound() {
+  return <MdxLayout>
+    <h1>404</h1>
+    <p>Looks like the page you were looking for is on sabbatical.</p>
+  </MdxLayout>
+}
